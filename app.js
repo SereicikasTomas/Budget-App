1f7fefc6e3be42d5810f0d8774c6d7db94f889cc
jacando.tos <sereicikas.t@gmail.com>
2019-11-02 18:27:04 +0100

    group input fields

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ let UIController = (function() {
   let DOMstrings = {
     inputType: ".add__type",
     inputDescription: ".add__description",
-    inputValue: ".add__value"
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
   };
 
   return {
@@ -18,12 +19,17 @@ let UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
     }
   };
 })();
 
 //GLOBAL APP CONTROLLER
 let controller = (function(budgetCntrl, UICtrl) {
+  let DOM = UICtrl.getDOMstrings();
+
   let ctrlAddItem = function() {
     // 1. Get the filed input data
     let input = UICtrl.getInput();
@@ -33,7 +39,8 @@ let controller = (function(budgetCntrl, UICtrl) {
     // 4. Calculate the budget
     // 5. Display the budget on the UI
   };
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
