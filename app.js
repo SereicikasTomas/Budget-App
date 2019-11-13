f5c99021c6c8628cfa5914f2ca37b971529dec3a
jacando.tos <sereicikas.t@gmail.com>
2019-11-13 18:31:46 +0100

    update placeholders with data

--- app.js
+++ app.js
@@ -71,13 +71,13 @@ let UIController = (function() {
       };
     },
     addlistItem: function(obj, type) {
-      let html;
+      let html, newHtml;
       // Create HTML string with placeholder string
       if (type === "inc") {
-        html = `<div class="item clearfix" id="income-0">
-        <div class="item__description">Salary</div>
+        html = `<div class="item clearfix" id="income-%id%">
+        <div class="item__description">%description%</div>
         <div class="right clearfix">
-          <div class="item__value">+ 2,100.00</div>
+          <div class="item__value">%value%</div>
           <div class="item__delete">
             <button class="item__delete--btn">
               <i class="ion-ios-close-outline"></i>
@@ -85,11 +85,11 @@ let UIController = (function() {
           </div>
         </div>
       </div>`;
-      } else {
-        html = `<div class="item clearfix" id="expense-0">
-        <div class="item__description">Apartment rent</div>
+      } else if (type === "exp") {
+        html = `<div class="item clearfix" id="expense-%id%">
+        <div class="item__description">%description%</div>
         <div class="right clearfix">
-          <div class="item__value">- 900.00</div>
+          <div class="item__value">%value%</div>
           <div class="item__percentage">21%</div>
           <div class="item__delete">
             <button class="item__delete--btn">
@@ -100,6 +100,9 @@ let UIController = (function() {
       </div>`;
       }
       // Replace placeholder with actual data
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
       // Insert HTML into DOM
     },
     getDOMstrings: function() {
