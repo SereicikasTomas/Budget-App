b1c04bcb1bc136e24f5ceb697b161b19919f14b9
jacando.tos <sereicikas.t@gmail.com>
2019-11-27 10:57:59 +0100

    add setter and getter for pecentages

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ let budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
+  };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+    if (totalIncome > 0) {
+      this.percentage = Math.round((this.value * 100) / totalIncome);
+    } else {
+      this.percentage = -1;
+    }
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
   };
 
   let Income = function(id, description, value) {
